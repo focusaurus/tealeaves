c0d01f5c3581e2471b24d50ebd755936ec5ac9be
Peter Lyons <pete@peterlyons.com>
2017-11-30 10:31:34 -0700

    Clean up and get key count

--- src/bin/keyhole.rs
+++ src/bin/keyhole.rs
@@ -6,15 +6,6 @@ use std::fs;
 use std::io;
 use std::io::{BufRead, Read};
 use std::iter::Iterator;
-//
-// fn to_u32(bytes: &[u8]) -> u32 {
-//     let mut size = 0u32;
-//     for (index, &byte) in bytes.iter().enumerate() {
-//         size = size + byte as u32;
-//         if index < bytes.len() -1 {size = size << 8;}
-//     }
-//     size
-// }
 
 fn read_string<R: ReadBytesExt + Read>(reader: &mut R) -> String {
     let len = reader.read_u32::<BigEndian>().unwrap();
@@ -33,7 +24,6 @@ fn keyhole() -> io::Result<()> {
             .map(|l| l.unwrap())
             .filter(|line| !line.starts_with("-"))
             .collect();
-        // println!("{}", base64);
         let bytes = base64::decode(&base64).unwrap();
         let prefix = b"openssh-key-v1";
         let starts_with_prefix = bytes.len() >= prefix.len() && prefix == &bytes[0..prefix.len()];
@@ -41,69 +31,27 @@ fn keyhole() -> io::Result<()> {
             return Ok(());
         }
         println!("✓openssh-key-v1");
+
+        /*
+        byte[]	AUTH_MAGIC
+        string	ciphername
+        string	kdfname
+        string	kdfoptions
+        int	number of keys N
+        string	publickey1
+        string	publickey2
+*/
+
         // Make a reader for everything after the prefix plus the null byte
         let mut reader = io::BufReader::new(&bytes[prefix.len() + 1..]);
         let cipher_name = read_string(&mut reader);
         println!("cipher: {}", cipher_name);
         let kdfname = read_string(&mut reader);
         println!("kdfname: {}", kdfname);
-        // let reader = io::BufReader::new(&bytes[prefix.len()..]).bytes();
-        // let string_len: Vec<u8> = reader.take(4).map(|b|b.unwrap()).collect();
-
-        // let mut string_len = [0u8;4];
-        // reader.by_ref().read_exact(&mut string_len);
-        // let cipher_name_len = to_u32(&payload[0..5]);
-        // let mut string_len = [0u8;4];
-        // let cipher_name: Vec<u8> = reader.take(cipher_name_len as usize).map(|b|b.unwrap()).collect();
-        //
-        // reader.read_exact(&mut string_len);
-        // let payload = &bytes[prefix.len()..];
-        // let mut index = 0;
-        // index = index + 5;
-        // println!("cipher_name_len {}", cipher_name_len);
-        // let cipher_name = String::from_utf8_lossy(&payload[index..(index + cipher_name_len as usize)]);
-        // println!("cipher_name {}", cipher_name);
-        // let mut cipher_name = [
-        // index = index + cipher_name_len;
-
-        // let asn = yasna::parse_ber_general(&payload, yasna::BERMode::Der, |reader| {
-        //     reader.read_sequence(|reader| {
-        //                              let cipher_name = try!(reader.next().read_string());
-        //                              // let b = try!(reader.next().read_bool());
-        //                              return Ok(cipher_name);
-        //                          })
-        // });
-        // // let mut fields = payload.split(|&byte| byte == 0);//.filter(|byte|byte.len() > 0);
-        // let cipher_name = fields.next().unwrap();
-        // println!("cipher: {}", String::from_utf8_lossy(cipher_name));
-/*
-	byte[]	AUTH_MAGIC
-	string	ciphername
-	string	kdfname
-	string	kdfoptions
-	int	number of keys N
-	string	publickey1
-	string	publickey2
-*/
-
-
-        // for (index, word) in bytes.split(|&byte| byte == 0).filter(|byte|byte.len() > 0).enumerate() {
-        //     println!("{}: {} (len: {})", index, String::from_utf8_lossy(word), word.len());
-        // }
-        // println!("{:?}", bytes[23] == 0);
-        // println!("{}", String::from_utf8_lossy(&bytes[0..15]));
-        // let asn = yasna::parse_ber_general(&bytes, yasna::BERMode::Der, |reader| {
-        //     reader.read_sequence(|reader| {
-        //                              Ok(())
-        //                              // let i = try!(reader.next().read_i64());
-        //                              // let b = try!(reader.next().read_bool());
-        //                              // return Ok((i, b));
-        //                          })
-        // });
-        // match asn {
-        //     Ok(_) => println!("OK"),
-        //     Err(error) => println!("Err {}", error),
-        // }
+        let kdfoptions = read_string(&mut reader);
+        println!("kdfoptions: {}", kdfoptions);
+        let pub_key_count = reader.read_u32::<BigEndian>().unwrap();
+        println!("key count {}", pub_key_count);
 
     }
     Ok(())
